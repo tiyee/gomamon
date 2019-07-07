dcc2a38835f9a10e05ec49ca391642c9d2de5ef7
tiyee <tiyee@live.com>
2019-07-07 22:27:26 +0800

    nav flex mode

--- src/index.tsx
+++ src/index.tsx
@@ -22,6 +22,9 @@ interface IApp {
   nav: string;
   snd: string;
   main: string;
+  y: number;
+  w: number;
+  h: number;
 }
 
 class App extends React.Component<{}, IApp> {
@@ -31,62 +34,100 @@ class App extends React.Component<{}, IApp> {
     list: [],
     nav: '',
     snd: '',
-    main: ''
+    main: '',
+    y: 0,
+    h: 800,
+    w: 1000
+  };
+
+  public componentWillUnmount = () => {
+    const _this = this;
+    window.removeEventListener('scroll', e => {
+      _this.handleScroll(e);
+    });
+  };
+  public handleScroll = (e: React.MouseEvent<Element, MouseEvent>) => {
+    // console.log(e.pageX, e.pageY);
+    const main = document.querySelector('#main');
+    let { h } = this.state;
+    if (main) {
+      const mainH = main.scrollHeight;
+      h = mainH;
+    }
+    const y = e.pageY;
+    if (y !== undefined) {
+      this.setState({ y, h });
+    }
   };
   public render(): JSX.Element {
-    const { items, list, nav, main, snd, init } = this.state;
+    const { items, list, nav, main, snd, init, y, w, h } = this.state;
+    const heightStypes: React.CSSProperties = {
+      height: `${h}px`
+    };
+    const navStypes: React.CSSProperties = {
+      width: `${w * 0.125}px`,
+      top: `${y}px`
+    };
+    const listStypes: React.CSSProperties = {
+      width: `${w * 0.25}px`,
+      top: `${y}px`
+    };
     return (
       <div id="layout">
-        <div id="nav">
-          <ul>
-            {items.map((item: IDocument) => {
-              const active = item.key === nav ? 'active' : '';
-              return (
-                <li key={item.key} className={active}>
-                  <a onClick={() => this._navClick(item)} href="#">
-                    {item.text}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-          <ul>
-            <li className="sign">
-              <span className="html" />
-              HTML
-            </li>
-            <li className="sign">
-              <span className="post" />
-              POST
-            </li>
-            <li className="sign">
-              <span className="get" />
-              GET
-            </li>
-          </ul>
+        <div id="nav" style={heightStypes}>
+          <div style={navStypes}>
+            <ul>
+              {items.map((item: IDocument) => {
+                const active = item.key === nav ? 'active' : '';
+                return (
+                  <li key={item.key} className={active}>
+                    <a onClick={(e: React.MouseEvent) => this._navClick(item)} href="#">
+                      {item.text}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+            <ul>
+              <li className="sign">
+                <span className="html" />
+                HTML
+              </li>
+              <li className="sign">
+                <span className="post" />
+                POST
+              </li>
+              <li className="sign">
+                <span className="get" />
+                GET
+              </li>
+            </ul>
+          </div>
         </div>
         {init ? (
           ''
         ) : (
           <div id="list">
-            {list.map((item: IList) => {
-              const active = item.url === snd ? 'active' : '';
-              return (
-                <dl
-                  onClick={() => {
-                    this._listClick(item);
-                  }}
-                  key={item.url}
-                  className={item.method + ' ' + active}
-                >
-                  <dt>{item.title}</dt>
-                  <dd>
-                    <span className="author">{item.author}</span>
-                    <span className="date">{item.date}</span>
-                  </dd>
-                </dl>
-              );
-            })}
+            <div style={listStypes}>
+              {list.map((item: IList) => {
+                const active = item.url === snd ? 'active' : '';
+                return (
+                  <dl
+                    onClick={() => {
+                      this._listClick(item);
+                    }}
+                    key={item.url}
+                    className={item.method + ' ' + active}
+                  >
+                    <dt>{item.title}</dt>
+                    <dd>
+                      <span className="author">{item.author}</span>
+                      <span className="date">{item.date}</span>
+                    </dd>
+                  </dl>
+                );
+              })}
+            </div>
           </div>
         )}
         {init ? (
@@ -117,7 +158,6 @@ class App extends React.Component<{}, IApp> {
           text: Buffer.from(item.file, 'base64').toString(),
           ts: 0,
           render: MDparse(cnt),
-
           list: SndList(MDparse(cnt))
         });
       });
@@ -131,10 +171,29 @@ class App extends React.Component<{}, IApp> {
         return response.json();
       })
       .then(fn);
+    const _this = this;
+    window.addEventListener('scroll', e => {
+      _this.handleScroll(e);
+    });
+    const body = document.querySelector('body');
+
+    let w = 2014;
+    let h = 800;
+    if (body) {
+      w = body.offsetWidth;
+      h = body.clientHeight;
+    }
+    this.setState({ w, h });
   };
+
   private _navClick = (item: IDocument) => {
     // event.preventDefault()
-    this.setState({ list: item.list, nav: item.key, main: item.render, init: false });
+    this.setState({
+      list: item.list,
+      nav: item.key,
+      main: item.render,
+      init: false
+    });
   };
   private _listClick = (item: IList) => {
     this.setState({ snd: item.url });
@@ -142,5 +201,4 @@ class App extends React.Component<{}, IApp> {
     location.href = '#' + Buffer.from(item.title, 'utf-8').toString('base64');
   };
 }
-
 ReactDOM.render(<App />, document.getElementById('root'));
