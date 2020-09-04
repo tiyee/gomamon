87ae5480546385b9cd1f20c7cc54476d6a051a22
tiyee <tiyee@live.com>
2020-09-04 22:47:19 +0800

    upgade dependency to latest version

--- src/components/common/md.tsx
+++ src/components/common/md.tsx
@@ -1,117 +1,119 @@
-import * as cheerio from 'cheerio';
-import marked from 'marked';
-import Prism from 'prismjs';
-String.prototype.trim = function(): string {
-  return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-};
+/** @format */
+
+import * as cheerio from 'cheerio'
+import marked from 'marked'
+import Prism from 'prismjs'
+String.prototype.trim = function (): string {
+    return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '')
+}
 const langSelect = (lan: string) => {
-  if (lan === undefined) {
-    lan = '';
-  }
-  switch (lan.toLowerCase()) {
-    case 'css':
-    case 'style':
-      return Prism.languages.css;
-    case 'html':
-      return Prism.languages.html;
-    case 'markup':
+    if (lan === undefined) {
+        lan = ''
+    }
+    switch (lan.toLowerCase()) {
+        case 'css':
+        case 'style':
+            return Prism.languages.css
+        case 'html':
+            return Prism.languages.html
+        case 'markup':
 
-    case 'xml':
-      return Prism.languages.xml;
-    case 'svg':
-      return Prism.languages.svg;
-    case 'mathml':
-      return Prism.languages.markup;
-    case 'js':
-    case 'javascript':
-    case 'json':
-    case 'python':
-    case 'go':
-    case 'ruby':
-      return Prism.languages.javascript;
-    case 'c':
-    case 'cpp':
-    case 'c/c++':
-    case 'c++':
-    case 'php':
-      return Prism.languages.clike;
-    default:
-      return Prism.languages.markup;
-  }
-};
-const renderer = new marked.Renderer();
+        case 'xml':
+            return Prism.languages.xml
+        case 'svg':
+            return Prism.languages.svg
+        case 'mathml':
+            return Prism.languages.markup
+        case 'js':
+        case 'javascript':
+        case 'json':
+        case 'python':
+        case 'go':
+        case 'ruby':
+            return Prism.languages.javascript
+        case 'c':
+        case 'cpp':
+        case 'c/c++':
+        case 'c++':
+        case 'php':
+            return Prism.languages.clike
+        default:
+            return Prism.languages.markup
+    }
+}
+const renderer = new marked.Renderer()
 renderer.code = (code: string, infostring: string, escaped: boolean) => {
-  // return `${code} ${infostring} ${escaped};`
-  // console.log(Prism.languages);
+    // return `${code} ${infostring} ${escaped};`
+    // console.log(Prism.languages);
 
-  const html = Prism.highlight(code, langSelect(infostring), infostring);
-  return `<pre><code class="language-${infostring}">${html}</code></pre>`;
-};
+    const html = Prism.highlight(code, langSelect(infostring), infostring)
+    return `<pre><code class="language-${infostring}">${html}</code></pre>`
+}
 renderer.heading = (text: string, level: number): string => {
-  const b = Buffer.from(text, 'utf-8');
+    const b = Buffer.from(text, 'utf-8')
 
-  const key = b.toString('base64');
+    const key = b.toString('base64')
 
-  return `
+    return `
             <h${level} id=${key}>
               ${text}
-            </h${level}>`;
-};
+            </h${level}>`
+}
 export interface IList {
-  url: string;
-  author: string;
-  date: string;
-  method: string;
-  title: string;
+    url: string
+    author: string
+    date: string
+    method: string
+    title: string
 }
 
 export function MDparse(md: string): string {
-  return marked(md, { renderer });
+    return marked(md, {renderer})
 }
 export function SndList(html: string): IList[] {
-  const $ = cheerio.load(html);
-  const items: IList[] = [];
-  $('h2').each((i, elem) => {
-    // console.log(elem.children[0].data);
-    let nex = elem.next;
-    while (nex) {
-      if (nex.name === 'ul') {
-        const item = {
-          url: '',
-          author: '',
-          date: '',
-          method: '', // method.lower(),
-          title: elem.children[0].data.trim()
-        };
-        nex.children.map(ele => {
-          if (ele.type === 'tag' && ele.name === 'li') {
-            ele.children.map(el => {
-              if (el.type === 'text') {
-                if (el.data.indexOf('接口地址') === 0) {
-                  // console.log('接口地址',el.next.children[0].data);
-                  item.url = el.next.children[0].data.trim();
+    const $ = cheerio.load(html)
+    const items: IList[] = []
+    $('h2').each((i, elem) => {
+        // console.log(elem.children[0].data);
+        let nex = elem.next
+        while (nex) {
+            if (nex.name === 'ul') {
+                const item = {
+                    url: '',
+                    author: '',
+                    date: '',
+                    method: '', // method.lower(),
+                    title: elem.children[0].data.trim(),
                 }
-                if (el.data.indexOf('请求方式') === 0) {
-                  // console.log('请求方式',el.next.children[0].data);
-                  item.method = el.next.children[0].data.trim().toLowerCase();
-                }
-                if (el.data.indexOf('联系人') === 0) {
-                  // console.log('联系人',el.next.children[0].data);
-                  item.author = el.next.children[0].data.trim();
-                }
-                if (el.data.indexOf('修改时') === 0) {
-                  // console.log('修改时',el.next.children[0].data);
-                  item.date = el.next.children[0].data.trim();
-                }
-              }
-            });
-          }
-        });
-        items.push(item);
-        break;
-      }
-      nex = nex.next;
-    }
-  });
-  return items;
+                nex.children.map(ele => {
+                    if (ele.type === 'tag' && ele.name === 'li') {
+                        ele.children.map(el => {
+                            if (el.type === 'text') {
+                                if (el.data.indexOf('接口地址') === 0) {
+                                    // console.log('接口地址',el.next.children[0].data);
+                                    item.url = el.next.children[0].data.trim()
+                                }
+                                if (el.data.indexOf('请求方式') === 0) {
+                                    // console.log('请求方式',el.next.children[0].data);
+                                    item.method = el.next.children[0].data.trim().toLowerCase()
+                                }
+                                if (el.data.indexOf('联系人') === 0) {
+                                    // console.log('联系人',el.next.children[0].data);
+                                    item.author = el.next.children[0].data.trim()
+                                }
+                                if (el.data.indexOf('修改时') === 0) {
+                                    // console.log('修改时',el.next.children[0].data);
+                                    item.date = el.next.children[0].data.trim()
+                                }
+                            }
+                        })
+                    }
+                })
+                items.push(item)
+                break
+            }
+            nex = nex.next
+        }
+    })
+    return items
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,204 +1,205 @@
-import { IList, MDparse, SndList } from 'components/common/md';
-import 'components/common/style.scss';
-import { checkStatus } from 'components/common/utils';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+/** @format */
+
+import {IList, MDparse, SndList} from './components/common/md'
+import 'components/common/style.scss'
+import {checkStatus} from './components/common/utils'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 interface IRest {
-  ts: number;
-  md: string;
-  file: string;
+    ts: number
+    md: string
+    file: string
 }
 interface IDocument {
-  key: string;
-  text: string;
-  ts: number;
-  render: string;
-  list: IList[];
+    key: string
+    text: string
+    ts: number
+    render: string
+    list: IList[]
 }
 interface IApp {
-  init: boolean;
-  items: IDocument[];
-  list: IList[];
-  nav: string;
-  snd: string;
-  main: string;
-  y: number;
-  w: number;
-  h: number;
+    init: boolean
+    items: IDocument[]
+    list: IList[]
+    nav: string
+    snd: string
+    main: string
+    y: number
+    w: number
+    h: number
 }
 
 class App extends React.Component<{}, IApp> {
-  public state: IApp = {
-    init: true,
-    items: [],
-    list: [],
-    nav: '',
-    snd: '',
-    main: '',
-    y: 0,
-    h: 800,
-    w: 1000
-  };
+    public state: IApp = {
+        init: true,
+        items: [],
+        list: [],
+        nav: '',
+        snd: '',
+        main: '',
+        y: 0,
+        h: 800,
+        w: 1000,
+    }
 
-  public componentWillUnmount = () => {
-    const _this = this;
-    window.removeEventListener('scroll', e => {
-      _this.handleScroll();
-    });
-  };
-  public handleScroll = () => {
-    // console.log(e.pageX, e.pageY);
-    const main = document.querySelector('#main');
-    let { h } = this.state;
-    if (main) {
-      const mainH = main.scrollHeight;
-      h = mainH;
+    public componentWillUnmount = () => {
+        const _this = this
+        window.removeEventListener('scroll', e => {
+            _this.handleScroll()
+        })
     }
-    const y = window.pageYOffset;
-    if (y !== undefined) {
-      this.setState({ y, h });
+    public handleScroll = () => {
+        // console.log(e.pageX, e.pageY);
+        const main = document.querySelector('#main')
+        let {h} = this.state
+        if (main) {
+            const mainH = main.scrollHeight
+            h = mainH
+        }
+        const y = window.pageYOffset
+        if (y !== undefined) {
+            this.setState({y, h})
+        }
     }
-  };
-  public render(): JSX.Element {
-    const { items, list, nav, main, snd, init, y, w, h } = this.state;
-    const heightStypes: React.CSSProperties = {
-      height: `${h}px`
-    };
-    const navStypes: React.CSSProperties = {
-      width: `${w * 0.125}px`,
-      top: `${y}px`
-    };
-    const listStypes: React.CSSProperties = {
-      width: `${w * 0.25}px`,
-      top: `${y}px`
-    };
-    return (
-      <div id="layout">
-        <div id="nav" style={heightStypes}>
-          <div style={navStypes}>
-            <ul>
-              {items.map((item: IDocument) => {
-                const active = item.key === nav ? 'active' : '';
-                return (
-                  <li key={item.key} className={active}>
-                    <a onClick={(e: React.MouseEvent) => this._navClick(item)} href="#">
-                      {item.text}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-            <ul>
-              <li className="sign">
-                <span className="html" />
-                HTML
-              </li>
-              <li className="sign">
-                <span className="post" />
-                POST
-              </li>
-              <li className="sign">
-                <span className="get" />
-                GET
-              </li>
-            </ul>
-          </div>
-        </div>
-        {init ? (
-          ''
-        ) : (
-          <div id="list">
-            <div style={listStypes}>
-              {list.map((item: IList) => {
-                const active = item.url === snd ? 'active' : '';
-                return (
-                  <dl
-                    onClick={() => {
-                      this._listClick(item);
-                    }}
-                    key={item.url}
-                    className={item.method + ' ' + active}
-                  >
-                    <dt>{item.title}</dt>
-                    <dd>
-                      <span className="author">{item.author}</span>
-                      <span className="date">{item.date}</span>
-                    </dd>
-                  </dl>
-                );
-              })}
+    public render(): JSX.Element {
+        const {items, list, nav, main, snd, init, y, w, h} = this.state
+        const heightStypes: React.CSSProperties = {
+            height: `${h}px`,
+        }
+        const navStypes: React.CSSProperties = {
+            width: `${w * 0.125}px`,
+            top: `${y}px`,
+        }
+        const listStypes: React.CSSProperties = {
+            width: `${w * 0.25}px`,
+            top: `${y}px`,
+        }
+        return (
+            <div id='layout'>
+                <div id='nav' style={heightStypes}>
+                    <div style={navStypes}>
+                        <ul>
+                            {items.map((item: IDocument) => {
+                                const active = item.key === nav ? 'active' : ''
+                                return (
+                                    <li key={item.key} className={active}>
+                                        <a onClick={(e: React.MouseEvent) => this._navClick(item)} href='#'>
+                                            {item.text}
+                                        </a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <ul>
+                            <li className='sign'>
+                                <span className='html' />
+                                HTML
+                            </li>
+                            <li className='sign'>
+                                <span className='post' />
+                                POST
+                            </li>
+                            <li className='sign'>
+                                <span className='get' />
+                                GET
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                {init ? (
+                    ''
+                ) : (
+                    <div id='list'>
+                        <div style={listStypes}>
+                            {list.map((item: IList) => {
+                                const active = item.url === snd ? 'active' : ''
+                                return (
+                                    <dl
+                                        onClick={() => {
+                                            this._listClick(item)
+                                        }}
+                                        key={item.url}
+                                        className={item.method + ' ' + active}>
+                                        <dt>{item.title}</dt>
+                                        <dd>
+                                            <span className='author'>{item.author}</span>
+                                            <span className='date'>{item.date}</span>
+                                        </dd>
+                                    </dl>
+                                )
+                            })}
+                        </div>
+                    </div>
+                )}
+                {init ? (
+                    ''
+                ) : (
+                    <div id='main'>
+                        <div dangerouslySetInnerHTML={{__html: main}} />
+                    </div>
+                )}
+                {init ? (
+                    <div id='mask'>
+                        <img src='./gomamon.jpg' />
+                    </div>
+                ) : (
+                    ''
+                )}
             </div>
-          </div>
-        )}
-        {init ? (
-          ''
-        ) : (
-          <div id="main">
-            <div dangerouslySetInnerHTML={{ __html: main }} />
-          </div>
-        )}
-        {init ? (
-          <div id="mask">
-            <img src="./gomamon.jpg" />
-          </div>
-        ) : (
-          ''
-        )}
-      </div>
-    );
-  }
-  public componentDidMount = () => {
-    const fn = (d: IRest[]) => {
-      const items: IDocument[] = [];
-      d.map((item: IRest) => {
-        const cnt = Buffer.from(item.md, 'base64').toString();
-
-        items.push({
-          key: item.file,
-          text: Buffer.from(item.file, 'base64').toString(),
-          ts: 0,
-          render: MDparse(cnt),
-          list: SndList(MDparse(cnt))
-        });
-      });
-      this.setState({ items });
-    };
-    fetch('/doc.json?' + new Date().getSeconds().toString(), {
-      method: 'GET'
-    })
-      .then(checkStatus)
-      .then((response: Response) => {
-        return response.json();
-      })
-      .then(fn);
-    const _this = this;
-    window.addEventListener('scroll', e => {
-      _this.handleScroll();
-    });
-    const body = document.querySelector('body');
-
-    let w = 2014;
-    let h = 800;
-    if (body) {
-      w = body.offsetWidth;
-      h = body.clientHeight;
+        )
     }
-    this.setState({ w, h });
-  };
+    public componentDidMount = () => {
+        const fn = (d: IRest[]) => {
+            const items: IDocument[] = []
+            d.map((item: IRest) => {
+                const cnt = Buffer.from(item.md, 'base64').toString()
 
-  private _navClick = (item: IDocument) => {
-    // event.preventDefault()
-    this.setState({
-      list: item.list,
-      nav: item.key,
-      main: item.render,
-      init: false
-    });
-  };
-  private _listClick = (item: IList) => {
-    this.setState({ snd: item.url });
-    console.log(item.title);
-    location.href = '#' + Buffer.from(item.title, 'utf-8').toString('base64');
-  };
+                items.push({
+                    key: item.file,
+                    text: Buffer.from(item.file, 'base64').toString(),
+                    ts: 0,
+                    render: MDparse(cnt),
+                    list: SndList(MDparse(cnt)),
+                })
+            })
+            this.setState({items})
+        }
+        fetch('/doc.json?' + new Date().getSeconds().toString(), {
+            method: 'GET',
+        })
+            .then(checkStatus)
+            .then((response: Response) => {
+                return response.json()
+            })
+            .then(fn)
+        const _this = this
+        window.addEventListener('scroll', e => {
+            _this.handleScroll()
+        })
+        const body = document.querySelector('body')
+
+        let w = 2014
+        let h = 800
+        if (body) {
+            w = body.offsetWidth
+            h = body.clientHeight
+        }
+        this.setState({w, h})
+    }
+
+    private _navClick = (item: IDocument) => {
+        // event.preventDefault()
+        this.setState({
+            list: item.list,
+            nav: item.key,
+            main: item.render,
+            init: false,
+        })
+    }
+    private _listClick = (item: IList) => {
+        this.setState({snd: item.url})
+        console.log(item.title)
+        location.href = '#' + Buffer.from(item.title, 'utf-8').toString('base64')
+    }
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
