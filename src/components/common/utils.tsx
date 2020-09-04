29b3af7cdc56b3c9ce29bc0fa363ffd2c9cb1473
tiyee <tiyee@live.com>
2020-09-04 22:37:45 +0800

    upgade dependency to latest version

--- src/components/common/utils.tsx
+++ src/components/common/utils.tsx
@@ -1,70 +1,72 @@
-import { stringify } from 'qs';
+/** @format */
+
+import {stringify} from 'qs'
 export interface JsonResult {
-  error: number;
-  result?: any;
-  msg: string;
+    error: number
+    result?: any
+    msg: string
 }
 export function checkStatus(response: Response): Response {
-  if (response.status >= 200 && response.status < 300) {
-    return response;
-  } else {
-    const error = new Error(response.statusText);
-    throw error;
-  }
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        const error = new Error(response.statusText)
+        throw error
+    }
 }
 export function checkErrorCode(code: number): void {
-  if (code === 403) {
-    location.href = '/login.shtml';
-  }
+    if (code === 403) {
+        location.href = '/login.shtml'
+    }
 }
 export function contentType(key: 'json' | 'normal' | 'multipart'): Record<string, string> {
-  switch (key) {
-    case 'json':
-      return { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' };
-    case 'normal':
-      return { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' };
-    case 'multipart':
-      return { 'Content-Type': 'multipart/form-data' };
+    switch (key) {
+        case 'json':
+            return {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+        case 'normal':
+            return {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+        case 'multipart':
+            return {'Content-Type': 'multipart/form-data'}
 
-    default:
-      return { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' };
-  }
+        default:
+            return {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'}
+    }
 }
 export function qs(dic: object): string {
-  return stringify(dic);
+    return stringify(dic)
 }
 export function httpClient(
-  url: string,
-  method: 'GET' | 'POST',
-  headers: Record<string, string>,
-  body: string | FormData,
-  fn: (value: JsonResult) => any
+    url: string,
+    method: 'GET' | 'POST',
+    headers: Record<string, string>,
+    body: string | FormData,
+    fn: (value: JsonResult) => any,
 ): void {
-  let chain: Promise<Response>;
-  switch (method) {
-    case 'GET':
-      chain = fetch(url, {
-        credentials: 'same-origin',
-        method: 'GET',
-        headers
-      });
-      break;
-    case 'POST':
-      console.log(body);
-      chain = fetch(url, {
-        credentials: 'same-origin',
-        method: 'POST',
-        headers,
-        body
-      });
-      break;
-    default:
-      throw new Error('undefined method');
-  }
-  chain
-    .then(checkStatus)
-    .then((response: Response) => {
-      return response.json();
-    })
-    .then(fn);
+    let chain: Promise<Response>
+    switch (method) {
+        case 'GET':
+            chain = fetch(url, {
+                credentials: 'same-origin',
+                method: 'GET',
+                headers,
+            })
+            break
+        case 'POST':
+            console.log(body)
+            chain = fetch(url, {
+                credentials: 'same-origin',
+                method: 'POST',
+                headers,
+                body,
+            })
+            break
+        default:
+            throw new Error('undefined method')
+    }
+    chain
+        .then(checkStatus)
+        .then((response: Response) => {
+            return response.json()
+        })
+        .then(fn)
 }
